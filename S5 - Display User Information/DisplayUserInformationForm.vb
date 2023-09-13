e9dc4074afe9f9112b5eb65309e67e872a2c4c6e
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-12 22:56:10 -0600

    Updated Comments

--- S5 - Display User Information/DisplayUserInformationForm.vb
+++ S5 - Display User Information/DisplayUserInformationForm.vb
@@ -12,11 +12,13 @@ Public Class DisplayUserInformationForm
 
     'When the user presses the display button
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+        'This concatinates text from all input textboxes and outputs into the final output textbox
         FinalOutputTextBox.Text = NameTextBox.Text & ", " & AddressTextBox.Text() & ", " & PhoneTextBox.Text() & ", " & BirthdateTextBox.Text
     End Sub
 
     'When the user presses the display but backwards button
     Private Sub DisplayButBackwardsButton_Click(sender As Object, e As EventArgs) Handles DisplayButBackwardsButton.Click
+        'This concatinates text from all input textboxes, backwards, and outputs into the final output textbox
         FinalOutputTextBox.Text = BirthdateTextBox.Text & ", " & PhoneTextBox.Text & ", " & AddressTextBox.Text & ", " & NameTextBox.Text
     End Sub
 
